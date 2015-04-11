1c3100f9d8328d09d733deb45b71e10ba0aa5267
cocosily <zhyg1608@gmail.com>
2015-04-11 15:58:51 +0800

    ad dao update

--- src/com/zhongrun/dao/impl/AdDao.java
+++ src/com/zhongrun/dao/impl/AdDao.java
@@ -4,18 +4,22 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.zhongrun.common.dao.hibernate4.BaseHibernateDao;
 import com.zhongrun.dao.IAdDao;
-import com.zhongrun.dao.hibernate4.BaseHibernateDao;
 import com.zhongrun.model.Ad;
 
 @Repository("adDao")
-public class AdDao extends BaseHibernateDao<Ad,Integer> implements IAdDao{
+public class AdDao extends BaseHibernateDao<Ad,Integer> implements IAdDao<Ad,Integer>{
 
 	@Override
 	public List<Ad> getAllAd() {
 		// TODO Auto-generated method stub
 //		this.getSession().createSQLQuery("").
-		return this.listAll();
+		String hql = "from Ad";
+		Object[] paramlist = new Object[]{};
+		List<Ad> list = this.listPage(hql, 2, 10, paramlist);
+		this.execteBulk(hql, paramlist);
+		return list;
 	}
 
 	
