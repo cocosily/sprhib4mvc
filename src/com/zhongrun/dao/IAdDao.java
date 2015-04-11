5af7ccc75b2c526756545f903dadf9181ac3fe1c
cocosily <zhyg1608@gmail.com>
2015-04-11 15:59:34 +0800

    update

--- src/com/zhongrun/dao/IAdDao.java
+++ src/com/zhongrun/dao/IAdDao.java
@@ -2,9 +2,10 @@ package com.zhongrun.dao;
 
 import java.util.List;
 
+import com.zhongrun.common.dao.IBaseDao;
 import com.zhongrun.model.Ad;
 
-public interface IAdDao {
+public interface IAdDao<M extends java.io.Serializable, PK extends java.io.Serializable> extends IBaseDao<M, PK>{
 
 	public List<Ad> getAllAd();
 }
