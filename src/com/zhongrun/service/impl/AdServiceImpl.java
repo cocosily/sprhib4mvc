f8e2a24ef69ebcf754261d3fd6964890890ccdc3
cocosily <zhyg1608@gmail.com>
2015-04-11 16:00:36 +0800

    update

--- src/com/zhongrun/service/impl/AdServiceImpl.java
+++ src/com/zhongrun/service/impl/AdServiceImpl.java
@@ -2,14 +2,11 @@ package com.zhongrun.service.impl;
 
 import java.util.List;
 
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.zhongrun.dao.IAdDao;
-import com.zhongrun.dao.impl.AdDao;
 import com.zhongrun.model.Ad;
 import com.zhongrun.service.IAdService;
 
@@ -19,11 +16,23 @@ public class AdServiceImpl implements IAdService {
 	@Autowired
 	@Qualifier("adDao")
 //	@Resource(name="adDao")
-	private IAdDao adDao;
+	private IAdDao<Ad,Integer> adDao;
 	
 	public List<Ad> getAdList(){
-		return adDao.getAllAd();
-		//return null;
+		
+		//return adDao.getAllAd();
+		return adDao.listAll();
 	}
+	
+	public Integer saveAd(Ad model){
+		return adDao.save(model);
+	}
+
+	@Override
+	public Integer saveAdM(Ad model) {
+		// TODO Auto-generated method stub
+		return adDao.save(model);
+	}
+
 	
 }
