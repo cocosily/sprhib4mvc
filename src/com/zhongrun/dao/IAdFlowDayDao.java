cdfadb98c73482dfba0e39abd7115d47ef5b629c
cocosily <zhyg1608@gmail.com>
2015-07-03 14:54:54 +0800

    adflowday dao interface

---
+++ src/com/zhongrun/dao/IAdFlowDayDao.java
@@ -0,0 +1,11 @@
+package com.zhongrun.dao;
+
+import java.util.List;
+
+import com.zhongrun.common.dao.IBaseDao;
+import com.zhongrun.model.Ad;
+
+public interface IAdFlowDayDao<M extends java.io.Serializable, PK extends java.io.Serializable> extends IBaseDao<M, PK>{
+
+	public List<Ad> getAllAd();
+}
